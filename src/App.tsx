d75368125fac6cc16556ec7c3c4fdd018096dc92
Taraslysak <lysak.ipr@gmail.com>
2022-11-07 10:01:27 +0200

    project skeleton

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const API_TOKEN = "fx9yP1GQJ_SsBh4ecq4k";
+
+function App() {
+  return (
+    <div className="app">
+      <button className="my-button">First API request</button>
+    </div>
+  );
+}
+
+export default App;
